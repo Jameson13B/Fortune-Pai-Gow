4ea31cf545dacc398b1b5544d7fe867b9d12d249
Jameson Brown <jamesonbrown@neolivity.com>
2025-12-29 13:58:19 -0700

    Refactor start commands in package.json and Procfile; update server build path in Server.js

--- src/lib/Server.js
+++ src/lib/Server.js
@@ -19,7 +19,7 @@ const server = Server({
 const PORT = process.env.PORT || 8000
 
 // Build path relative to the server.js file
-const frontEndAppBuildPath = path.resolve(__dirname, "./build")
+const frontEndAppBuildPath = path.resolve(__dirname, "../../dist")
 server.app.use(serve(frontEndAppBuildPath))
 
 server.run(PORT, () => {
